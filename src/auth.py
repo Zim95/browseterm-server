f5ed9dc7f3e659f895358a4616138d8ec2567a75
Namah Shrestha <namah.shrestha@tgs.com>
2024-04-07 03:45:21 +0530

    Added skeleton code for google auth validation

--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,11 @@
 # builtins
 import functools
+import time
+import os
 
 # third party
 import flask.sessions as sessions
+import google.auth as gauth
 
 
 class AuthValidator:
@@ -21,6 +24,24 @@ class GoogleAuthValidator(AuthValidator):
 
     def __init__(self) -> None:
         super().__init__()
+
+    def validate_session(self) -> bool:
+        try:
+            id_token: str = self.session.get("token_info", {}).get("id_token", "")
+            if not id_token:
+                return "No token available", False
+            id_info = gauth.jwt.decode(id_token, verify=False)
+            # Check if the token has expired
+            if id_info['exp'] < time.time():
+                return 'Token has expired', False
+            # Optionally, verify other claims such as audience and issuer
+            if id_info['aud'] != os.environ.get("GOOGLE_AUTH_CLIENT_ID"):
+                return 'Invalid client id', False
+            if id_info['iss'] != 'https://accounts.google.com':
+                return 'Invalid issuer', False
+            return "Verified", True
+        except Exception as e:
+            return str(e), False
 
 
 class GithubAuthValidator(AuthValidator):
