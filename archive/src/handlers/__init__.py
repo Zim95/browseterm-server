ad7bc2ebc7a4bfa128791283bf8a06e45de73bab
Zim95 <shresthanamah@gmail.com>
2025-10-10 17:04:29 +0530

    Removed archived code

--- archive/src/handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.handlers.token_exchange_handler import GoogleTokenExchangeHandler, GithubTokenExchangeHandler
-from src.handlers.login_redirect_handler import GoogleLoginRedirectHandler, GithubLoginRedirectHandler
-from src.handlers.image_options_handler import ImageOptionsHandler
-from src.handlers.login_handler import GoogleLoginHandler, GithubLoginHandler
-from src.handlers.ping_handler import PingHandler
-from src.handlers.base_handler import Handler
-from src.handlers.usl_handler import USLHandler
-from src.handlers.profile_handler import ProfileHandler
-from src.handlers.logout_handler import LogoutHandler
--- archive/src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# commons
-from src.models.commons import Base
-# models
-from src.models.user import Users
-# modelops
-from src.models.user import user_model_ops
