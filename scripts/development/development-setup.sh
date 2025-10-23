f82ecbbff688ecdea43d1918a3ac84a408393398
Zim95 <shresthanamah@gmail.com>
2025-10-23 20:46:23 +0530

    Added container maker host and port into development.yaml and development-setup.sh

--- scripts/development/development-setup.sh
+++ scripts/development/development-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if enough arguments are provided
-if [ $# -lt 3 ]; then
-    echo "Usage: $0 <namespace> <absolute-path-to-current-working-directory> <repo-name> <google-client-id> <google-client-secret> <github-client-id> <github-client-secret> <redis-host> <redis-port> <redis-password> <redis-username> <redis-db> <postgres-host> <postgres-port> <postgres-user> <postgres-password> <postgres-db>"
+if [ $# -lt 20 ]; then
+    echo "Usage: $0 <namespace> <absolute-path-to-current-working-directory> <repo-name> <container-maker-host> <container-maker-port> <auth-redirect-base-uri> <google-client-id> <google-client-secret> <github-client-id> <github-client-secret> <redis-host> <redis-port> <redis-password> <redis-username> <redis-db> <postgres-host> <postgres-port> <postgres-user> <postgres-password> <postgres-db>"
     exit 1
 fi
 
@@ -10,25 +10,29 @@ YAML=./infra/development/development.yaml
 NAMESPACE=$1
 HOSTPATH=$2
 REPO_NAME=$3
-AUTH_REDIRECT_BASE_URI=$4
-GOOGLE_CLIENT_ID=$5
-GOOGLE_CLIENT_SECRET=$6
-GITHUB_CLIENT_ID=$7
-GITHUB_CLIENT_SECRET=$8
-REDIS_HOST=$9
-REDIS_PORT=${10}
-REDIS_PASSWORD=${11}
-REDIS_USERNAME=${12}
-REDIS_DB=${13}
-POSTGRES_HOST=${14}
-POSTGRES_PORT=${15}
-POSTGRES_USER=${16}
-POSTGRES_PASSWORD=${17}
-POSTGRES_DB=${18}
+CONTAINER_MAKER_DEVELOPMENT_HOST=$4
+CONTAINER_MAKER_DEVELOPMENT_PORT=$5
+AUTH_REDIRECT_BASE_URI=$6
+GOOGLE_CLIENT_ID=$7
+GOOGLE_CLIENT_SECRET=$8
+GITHUB_CLIENT_ID=$9
+GITHUB_CLIENT_SECRET=${10}
+REDIS_HOST=${11}
+REDIS_PORT=${12}
+REDIS_PASSWORD=${13}
+REDIS_USERNAME=${14}
+REDIS_DB=${15}
+POSTGRES_HOST=${16}
+POSTGRES_PORT=${17}
+POSTGRES_USER=${18}
+POSTGRES_PASSWORD=${19}
+POSTGRES_DB=${20}
 
 export NAMESPACE=$NAMESPACE
 export HOSTPATH=$HOSTPATH
 export REPO_NAME=$REPO_NAME
+export CONTAINER_MAKER_DEVELOPMENT_HOST=$CONTAINER_MAKER_DEVELOPMENT_HOST
+export CONTAINER_MAKER_DEVELOPMENT_PORT=$CONTAINER_MAKER_DEVELOPMENT_PORT
 export AUTH_REDIRECT_BASE_URI=$AUTH_REDIRECT_BASE_URI
 export GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID
 export GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET
